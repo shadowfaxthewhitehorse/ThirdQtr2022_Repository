8acec5d0dedfc04a2ca304501e194aa18b856f6d
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-07-01 11:21:43 +0530

    Create README_versionInformation.py

---
+++ README_versionInformation.py
@@ -0,0 +1,9 @@
+# PYTHON CODE
+#
+# This file has the version information for:
+# - Python
+# 
+# Keep this file around so that we know what version of Python to use.
+# Ten years later, there will be a whole lot of new Python releases. So, we need to keep this file around.
+#
+print("Python version 3.8.10", end=" \n\n")
